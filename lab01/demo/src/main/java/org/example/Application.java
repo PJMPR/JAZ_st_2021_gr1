854f99e73b9af42cfd53e24095ce4768f7e781be
karmenix <s23215@pjwstk.edu.pl>
2021-10-12 18:40:48 +0200

    lab01

--- lab01/demo/src/main/java/org/example/Application.java
+++ lab01/demo/src/main/java/org/example/Application.java
@@ -1,8 +1,32 @@
 package org.example;
 
+import java.lang.reflect.Method;
+
 public class Application {
 
     public static void main(String[] args){
+        Class<?> czlowiek = Person.class;
 
+        printGettersSetters(czlowiek);
+    }
+    public static void printGettersSetters(Class Person){
+        Method[] methods = Person.getDeclaredMethods();
+
+        for(Method method : methods){
+            if(isGetter(method)) System.out.println("getter: " + method);
+            if(isSetter(method)) System.out.println("setter: " + method);
+        }
+    }
+    public static boolean isGetter(Method method){
+        if(!method.getName().startsWith("get"))      return false;
+        if(method.getParameterTypes().length != 0)   return false;
+        if(void.class.equals(method.getReturnType())) return false;
+        return true;
+    }
+
+    public static boolean isSetter(Method method){
+        if(!method.getName().startsWith("set")) return false;
+        if(method.getParameterTypes().length != 1) return false;
+        return true;
     }
 }
--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -3,13 +3,24 @@ package org.example;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz){
-        Method m = Arrays.stream(clazz.getDeclaredMethods()).findFirst().or(null).get();
+        List<Method> Getters = new ArrayList<Method>();
+        Method[] Method = clazz.getDeclaredMethods();
+
+        for (Method method: Method) {
+            if (method.getName().startsWith("is")||method.getName().startsWith("get")){
+                class.
+            }
+        }
+
+
 
 
         return Arrays.stream(clazz.getDeclaredMethods()).toList();
