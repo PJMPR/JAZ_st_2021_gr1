f2f13e92f5f014fc4bb6a8f670499d5137f59029
s23317 <s23317@pjwstk.edu.pl>
2021-11-04 00:29:08 +0100

    done

--- lab03/demo/src/main/java/org/example/queries/QueryProcessor.java
+++ lab03/demo/src/main/java/org/example/queries/QueryProcessor.java
@@ -1,15 +1,36 @@
 package org.example.queries;
 
+import org.example.filter_rules.*;
+import org.example.functions.AgeIncomeMethods;
 import org.example.model.People;
 import org.example.queries.results.Results;
 import org.example.queries.search.SearchParameters;
 
-public class QueryProcessor {
+import java.util.List;
 
-    public Results GetResults(SearchParameters parameters){
+
+public class QueryProcessor {
+    List<FilterInterface> filterInterfaceList = List.of(
+            new FromAgeRule(),
+            new FromIncRule(),
+            new GenderRule(),
+            new NameRule(),
+            new SurnameRule(),
+            new ToAgeRule(),
+            new ToIncRule(),
+            new PageSet()
+    );
+
+    public Results GetResults(SearchParameters parameters) {
         Results result = new Results();
 
+
+        AgeIncomeMethods function = new AgeIncomeMethods();
         result.setItems(People.Data);
+
+        filterInterfaceList.forEach(filter -> filter.useFilterInterface(result, parameters));
+
+        function.ageIncomeFunction(parameters, result);
 
         return result;
     }
