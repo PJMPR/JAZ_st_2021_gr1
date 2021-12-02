254523a65246fba0dca6236ed0b3b900e37164f6
Rigir <rafal.paszkowski.kontakt@gmail.com>
2021-12-02 14:04:52 +0100

    Move LAB07 to docker

---
+++ lab07/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.persistence.EntityManager;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
