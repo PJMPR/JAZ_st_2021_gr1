2c32387eaafe7d447c7f783ffc5efccaac631192
s23215 <s23215@pjwstk.edu.pl>
2021-12-21 22:36:28 +0100

    lab09 poprawna wersja

--- lab09/demo/demo/src/main/java/com/example/demo/contracts/FilmDto.java
+++ lab09/demo/demo/src/main/java/com/example/demo/contracts/FilmDto.java
@@ -1,17 +1,14 @@
 package com.example.demo.contracts;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
-<<<<<<< HEAD
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class FilmDto {
     private Integer id;
     private String title;
@@ -20,29 +17,5 @@ public class FilmDto {
     private BigDecimal rentalDuration;
     private BigDecimal rentalRate;
     private BigDecimal replacementCosts;
-}
-=======
-@AllArgsConstructor
-@NoArgsConstructor
-public class FilmDto {
-    private int id;
-    private String title;
-    private int releaseYear;
-    private int rentalDuration;
-    private BigDecimal rentalRate;
-    private BigDecimal replacementCosts;
-    private LanguageDto language;
 
 }
-/*
-export interface Film{
-    id:number,
-    title:string,
-    releaseYear?:number,
-    language?:Language,
-    rentalDuration?:number,
-    rentalRate?:number,
-    replacementCosts?:number
-}
-* */
->>>>>>> 62e3a4e14484aa83fc53cd8bd6a8184df1b08217
