1e62e1f736b887d9664f72985bbf67ca275af243
s23215 <s23215@pjwstk.edu.pl>
2021-12-21 22:36:28 +0100

    lab09 poprawna wersja

--- lab09/demo/demo/src/main/java/com/example/demo/model/Language.java
+++ lab09/demo/demo/src/main/java/com/example/demo/model/Language.java
@@ -2,22 +2,12 @@ package com.example.demo.model;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
-import java.util.Collection;
-<<<<<<< HEAD
-=======
-import java.util.Objects;
->>>>>>> 62e3a4e14484aa83fc53cd8bd6a8184df1b08217
 
 @Entity
 public class Language {
     private int languageId;
     private String name;
     private Timestamp lastUpdate;
-<<<<<<< HEAD
-    private Collection<Film> filmsByLanguageId;
-=======
-    private Collection<Film> films;
->>>>>>> 62e3a4e14484aa83fc53cd8bd6a8184df1b08217
 
     @Id
     @Column(name = "language_id")
@@ -53,7 +43,6 @@ public class Language {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-<<<<<<< HEAD
 
         Language language = (Language) o;
 
@@ -62,41 +51,13 @@ public class Language {
         if (lastUpdate != null ? !lastUpdate.equals(language.lastUpdate) : language.lastUpdate != null) return false;
 
         return true;
-=======
-        Language language = (Language) o;
-        return languageId == language.languageId && Objects.equals(name, language.name) && Objects.equals(lastUpdate, language.lastUpdate);
->>>>>>> 62e3a4e14484aa83fc53cd8bd6a8184df1b08217
     }
 
     @Override
     public int hashCode() {
-<<<<<<< HEAD
         int result = languageId;
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (lastUpdate != null ? lastUpdate.hashCode() : 0);
         return result;
     }
-
-    @OneToMany(mappedBy = "language")
-    public Collection<Film> getFilmsByLanguageId() {
-        return filmsByLanguageId;
-    }
-
-    public void setFilmsByLanguageId(Collection<Film> filmsByLanguageId) {
-        this.filmsByLanguageId = filmsByLanguageId;
-    }
 }
-=======
-        return Objects.hash(languageId, name, lastUpdate);
-    }
-
-    @OneToMany(mappedBy = "language")
-    public Collection<Film> getFilms() {
-        return films;
-    }
-
-    public void setFilms(Collection<Film> films) {
-        this.films = films;
-    }
-}
->>>>>>> 62e3a4e14484aa83fc53cd8bd6a8184df1b08217
